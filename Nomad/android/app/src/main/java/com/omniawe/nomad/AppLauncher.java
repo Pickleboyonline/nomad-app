557d7e58e81fb25945be06646eaf519942fb6473
Pickleboyonline <pickleboyonline@gmail.com>
2017-03-25 17:21:57 -0500

    Stuff

--- Nomad/android/app/src/main/java/com/omniawe/nomad/AppLauncher.java
+++ Nomad/android/app/src/main/java/com/omniawe/nomad/AppLauncher.java
@@ -1,11 +1,14 @@
 package com.omniawe.nomad;
 
+import android.content.Intent;
+
 import com.facebook.react.ReactPackage;
 import com.omniawe.nomad.generated.ExponentBuildConstants;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import host.exp.expoview.ExponentActivity;
 
@@ -17,7 +20,9 @@ public class AppLauncher extends ExponentActivity {
 
   @Override
   public String publishedUrl() {
-
+    Intent intent = getIntent();
+    String url = intent.getDataString();
+    final Map<String, String> data = parser(url);
     return "exp://exp.host/@pickleboyonline/nomad";
   }
 
