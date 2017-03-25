8d49d337f63bb6ec0cd749c7b1da2e2dcceaa760
Pickleboyonline <pickleboyonline@gmail.com>
2017-03-25 13:56:14 -0500

    Things added

---
+++ Nomad/android/app/src/main/java/com/omniawe/nomad/AppLauncher.java
@@ -0,0 +1,38 @@
+package com.omniawe.nomad;
+
+import com.facebook.react.ReactPackage;
+import com.omniawe.nomad.generated.ExponentBuildConstants;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import host.exp.expoview.ExponentActivity;
+
+public class AppLauncher extends ExponentActivity {
+
+  @Override
+  public String publishedUrl() {
+    return "exp://exp.host/@pickleboyonline/nomad";
+  }
+
+  @Override
+  public String developmentUrl() {
+    return ExponentBuildConstants.DEVELOPMENT_URL;
+  }
+
+  @Override
+  public List<String> sdkVersions() {
+    return new ArrayList<>(Arrays.asList("15.0.0"));
+  }
+
+  @Override
+  public List<ReactPackage> reactPackages() {
+    return ((MainApplication) getApplication()).getPackages();
+  }
+
+  @Override
+  public boolean isDebug() {
+    return BuildConfig.DEBUG;
+  }
+}
