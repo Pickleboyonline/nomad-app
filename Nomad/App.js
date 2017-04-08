291a86225d1b9e4fe88f01ff0ab8540b176e43f8
Pickleboyonline <pickleboyonline@gmail.com>
2017-04-08 11:47:21 -0500

    blurb

--- Nomad/App.js
+++ Nomad/App.js
@@ -5,12 +5,13 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open up Appjs to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Shake your phoneto open the developer menu.</Text>
       </View>
     );
   }
+  
 }
 
 const styles = StyleSheet.create({
