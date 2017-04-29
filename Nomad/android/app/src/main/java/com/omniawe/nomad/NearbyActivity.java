c15829f30e32fd8fc12b9de8d39c9a4429b87837
Pickleboyonline <pickleboyonline@gmail.com>
2017-04-29 17:15:10 -0500

    d

---
+++ Nomad/android/app/src/main/java/com/omniawe/nomad/NearbyActivity.java
@@ -0,0 +1,15 @@
+package com.omniawe.nomad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.omniawe.nomad.R;
+
+public class NearbyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nearby);
+    }
+}
